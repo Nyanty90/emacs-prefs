3f5532c687ff362d80460f5cd2f422ef0d7195dd
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2022-02-26 23:31:17 +0000

    Refactor common part of region-deletion sequence into a single function.

--- unhtml-wc.el
+++ unhtml-wc.el
@@ -10,50 +10,46 @@
             (
                 (curbuf (current-buffer))
                 (tempbuf (generate-new-buffer "wc-temp"))
+                (delete-between
+                    (lambda (find-opener find-closer)
+                        ; deletes region between points identified by
+                        ; find-opener and find-closer callbacks
+                        (goto-char (point-min))
+                        (while (funcall find-opener)
+                            (let*
+                                (
+                                    (end-tag (point))
+                                    (start-tag
+                                        (progn
+                                            (search-backward "<" nil nil)
+                                            (point)
+                                        ) ; progn
+                                    )
+                                )
+                                (goto-char end-tag)
+                                (funcall find-closer)
+                                (delete-region start-tag (point))
+                            ) ; let*
+                        ) ; while
+                    ) ; lambda
+                ) ; delete-between
                 count
             )
             (set-buffer tempbuf)
             (insert-buffer-substring-no-properties curbuf beg end)
             ; Multiline search seems to be troublesome (e.g. slow, stack overflow),
             ; so find opening and closing tags using separate searches.
-            (goto-char (point-min))
-            (while (search-forward "<!--" nil t)
-                ; Get rid of commented-out sections.
-                (let*
-                    (
-                        (end-tag (point))
-                        (start-tag
-                            (progn
-                                (search-backward "<" nil nil)
-                                (point)
-                            ) ; progn
-                        )
-                    )
-                    (goto-char end-tag)
-                    (search-forward "-->" nil nil)
-                    (delete-region start-tag (point))
-                ) ; let*
-            ) ; while
+            (funcall delete-between ; Get rid of commented-out sections.
+                (lambda () (search-forward "<!--" nil t))
+                (lambda () (search-forward "-->" nil nil))
+            )
             (dolist (tag '("head" "style" "script"))
                 ; Gobble entire content for these tags, up to and including closing tags
                 ; (luckily they don’t nest).
-                (goto-char (point-min))
-                (while (re-search-forward (concat "<" tag "[^>]*>") nil t)
-                    (let*
-                        (
-                            (end-tag (point))
-                            (start-tag
-                                (progn
-                                    (search-backward "<" nil nil)
-                                    (point)
-                                ) ; progn
-                            )
-                        )
-                        (goto-char end-tag)
-                        (re-search-forward (concat "</" tag "[^>]*>") nil nil)
-                        (delete-region start-tag (point))
-                    ) ; let*
-                ) ; while
+                (funcall delete-between
+                    (lambda () (re-search-forward (concat "<" tag "[^>]*>") nil t))
+                    (lambda () (re-search-forward (concat "</" tag "[^>]*>") nil nil))
+                )
             ) ; dolist
             (goto-char (point-min))
             (perform-replace "<[^>]+>" "" nil t nil)
